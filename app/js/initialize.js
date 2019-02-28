bd3990d79eeb638619b2778a7212e123be11ad6b
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2019-02-27 21:37:32 -0600

    Contact UI: Input hover & focus animations added; send button dot animation pending to refine.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -3,7 +3,19 @@
 // without waiting for stylesheets, images, and
 // subframes to finish loading
 document.addEventListener('DOMContentLoaded', function() {
-    // Do something
+    // Labels & input bottom line states
+	// Add/remove class to ".sz-formfield" to change behavior of its own label and bottom line
+		var sz_inputs = document.getElementsByClassName('sz-formfield');
+
+		for (var i = sz_inputs.length - 1; i >= 0; i--) {
+			sz_inputs[i].addEventListener('focusout', function(){
+				if (this.value){
+					this.classList.add('filled');
+				} else {
+					this.classList.remove('filled');
+				}
+			});
+		} 
 });
 
 
