472fa65a7025667d45335e8382f821ab00454638
Fantasma <fantasma@MacBook-Air-de-Fantasma.local>
2019-04-17 21:16:34 -0500

    Home & Cases UI: Filters show/hide interaction added.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -33,6 +33,59 @@ document.addEventListener('DOMContentLoaded', function() {
 				}
 			});
 		}
+
+	// Filters show/hide functionality
+		var Filters = {};
+		var pFilters = document.getElementById('proj-filters');
+		if (pFilters) {
+			Filters = {
+				areVisible: true,
+				categories: document.getElementsByClassName('filter'),
+				showTrigger: document.getElementById('filters-trigger'),
+
+				showListener: function(){
+					var _this = this;
+					this.showTrigger.addEventListener('click', function(){
+						_this.toggleFilters();
+					});
+				},
+				toggleFilters: function(){
+					if(this.areVisible){
+						this.hideFilters();
+					} else {
+						this.showFilters();
+					}
+				},
+
+				hideFilters: function(){
+					var _this = this;
+					for (var i = this.categories.length - 1; i >= 0; i--) {
+						_this.categories[i].classList.remove('show');
+					}
+
+					this.areVisible = false;
+				},
+				showFilters: function(){
+					var _this = this;
+					for (var i = this.categories.length - 1; i >= 0; i--) {
+						(function(i){
+							setTimeout(function(){
+								_this.categories[i].classList.add('show');
+							}, i * 120);
+						})(i);
+					}
+					this.areVisible = true;
+				},
+
+
+
+				init: function(){
+					this.showListener();
+				}
+			}
+
+			Filters.init();
+		}
 });
 
 
