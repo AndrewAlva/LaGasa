f092aafe74f1748b0b607946ceadba60dcf06164
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot-2.local>
2019-06-22 15:37:11 -0500

    Bio UI: Responsive layout done, interactions ready; pending pixel perfect tweaks.

--- app/js/bio.js
+++ app/js/bio.js
@@ -1,3 +1,5 @@
+// Other projects images canvas
+// img follow mouse position
 function Bio() {
     // Img shifter instances
     var opShifter = document.getElementById('op-shifter-1');
@@ -16,4 +18,16 @@ function Bio() {
         RAF.add(bio_ImgShifter_1);
         RAF.add(bio_ImgShifter_2);
     }
+}
+
+
+// Other projects list
+// Trigger to show/hide elements on mobile
+
+var ExtraProjectsTrigger = document.getElementById('toggle-extra-projects');
+if (ExtraProjectsTrigger) {
+    ExtraProjectsTrigger.addEventListener('click', function(e){
+        e.preventDefault();
+        ExtraProjectsTrigger.classList.toggle('show');
+    })
 }
